d09e32eef8209853967f2ce16d9b6e93fec819d6
Daisy Abuya <daizyabuya@gmil.com>
2022-04-13 12:03:54 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: nothing, return void
- *     */
+*main - Entry point
+*Return: nothing, return void
+*/
 
 int main(void)
 {
@@ -17,7 +18,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-         return (0);
-
+return (0);
 }
-
